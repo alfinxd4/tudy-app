0e8ca244ba803f18e9eace9b3c28388921af24e6
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2024-09-01 00:47:12 +0700

    config tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode : 'class',
+    content: [
+      './src/**/*.{html,js}',
+      './dist/**/*.html',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
+   
