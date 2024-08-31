72251e1395c3b9de53340d550a2fd9075982cdf9
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2024-09-01 01:17:56 +0700

    update config tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode : 'class',
+    darkMode : 'class', // config for dark mode system
     content: [
-      './src/**/*.{html,js}',
-      './dist/**/*.html',
+      './src/**/*.{html,js}', // entry point f find .html, .js in src
+      './dist/**/*.html', // entry point f .html, .js in src
     ],
     theme: {
       extend: {},
