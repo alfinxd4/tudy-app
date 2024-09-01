cab1d4207406f4b3ca27aac0e51f998ce948adc3
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2024-09-02 00:09:51 +0700

    update js

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,7 @@
+// import js
 import "../../src/css/main.css";
+
+// import img
 import "../assets/images/icon-splash.png";
 import "../assets/images/user1.jpg";
 import "../assets/images/user2.jpg";
@@ -6,23 +9,29 @@ import "../assets/images/user3.jpg";
 import "../assets/images/user4.jpg";
 import "../assets/images/user5.jpg";
 import "../assets/images/user6.jpg";
+import "../assets/images/work-icon.png";
+import "../assets/images/study-icon.png";
+import "../assets/images/sport-icon.png";
+import "../assets/images/fun-icon.png";
+import "../assets/images/family-icon.png";
+import "../assets/images/other-icon.png";
 
- 
+
 $(document).ready(function () {
   // get current.location.path
   const currentPage = window.location.pathname;
 
   // check username isExist
   const checkUser = () => {
-      if (currentPage === "/dist/todo.html") {
-          if (localStorage.getItem("user") === null) {
-              window.location.href = "index.html";
-          }
-      } else if (currentPage === "/dist/index.html" || currentPage === "/dist/") {
-          if (localStorage.getItem("user") !== null) {
-              window.location.href = "todo.html";
-          }
+    if (currentPage === "/dist/todo.html") {
+      if (localStorage.getItem("user") === null) {
+        window.location.href = "index.html";
       }
+    } else if (currentPage === "/dist/index.html" || currentPage === "/dist/") {
+      if (localStorage.getItem("user") !== null) {
+        window.location.href = "todo.html";
+      }
+    }
   };
 
   // get selector input username
@@ -31,20 +40,20 @@ $(document).ready(function () {
 
   // set username
   const setUser = () => {
-      localStorage.setItem("user", inputUserName.val().trim());
-      window.location.href = "todo.html";
+    localStorage.setItem("user", inputUserName.val().trim());
+    window.location.href = "todo.html";
   };
 
   // event setUser
   $(btnInputUserName).on("click", (e) => {
-      e.preventDefault();
+    e.preventDefault();
 
-      if (inputUserName.val().trim() === "") {
-          alert("Please enter your name.");
-          return;
-      }
+    if (inputUserName.val().trim() === "") {
+      alert("Please enter your name.");
+      return;
+    }
 
-      setUser();
+    setUser();
   });
 
   // event checkUser
@@ -52,8 +61,29 @@ $(document).ready(function () {
 
   // function date
   const currentDate = new Date();
-  let listDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  let listMonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let listDay = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let listMonth = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
   $("#current-day").text(currentDate.getDate());
   $("#current-month").text(listMonth[currentDate.getMonth()]);
@@ -62,63 +92,84 @@ $(document).ready(function () {
   // get selector username
   $("#username").text(localStorage.getItem("user"));
 
-//   const checkTotalTask = () => {
-//       const initializeTasks = (key) => {
-//           if (localStorage.getItem(key) === null) {
-//               localStorage.setItem(key, JSON.stringify([]));
-//           }
-//       };
-
-//       initializeTasks("taskWork");
-//       initializeTasks("taskStudy");
-//       initializeTasks("taskSport");
-//       initializeTasks("taskFun");
-//       initializeTasks("taskFamily");
-//       initializeTasks("taskOther");
-
-//       const updateTaskCount = (key, element) => {
-//           let taskArray = JSON.parse(localStorage.getItem(key));
-//           element.text(taskArray.length);
-//       };
-
-//       updateTaskCount("taskWork", $("#task-work"));
-//       updateTaskCount("taskStudy", $("#task-study"));
-//       updateTaskCount("taskSport", $("#task-sport"));
-//       updateTaskCount("taskFun", $("#task-fun"));
-//       updateTaskCount("taskFamily", $("#task-family"));
-//       updateTaskCount("taskOther", $("#task-other"));
-
-//       let countAllTask =  JSON.parse(localStorage.getItem("taskWork")) + 
-//                          JSON.parse(localStorage.getItem("taskStudy"));
-//       $("#total-all-task").text(countAllTask);
-//   };
-
-
-//  update totaltask
-const checkTotalTask = () => {
-    if (localStorage.getItem("user") === null) {
-        $("#task-work").text(localStorage.getItem("user"));
-    } else {
-      let taskArray = JSON.parse(localStorage.getItem('taskWork'))|| [];
-      // Hitung jumlah elemen dalam array
-      let taskCount = taskArray.length;
-      $("#task-work").text(taskCount);
+  // Inisialisasi localStorage with empty array
+  const initializeTasks = (key) => {
+    if (localStorage.getItem(key) === null) {
+      localStorage.setItem(key, JSON.stringify([])); // save as empty array
     }
-  
   };
-  
 
-// define task object data
-const generateTaskObject = (id,title,category,isCompleted) => {
+  // call function for all task
+  initializeTasks("taskWork");
+  initializeTasks("taskStudy");
+  initializeTasks("taskSport");
+  initializeTasks("taskFun");
+  initializeTasks("taskFamily");
+  initializeTasks("taskOther");
+
+  // Update count of task from localStorage
+  const updateTaskCount = (key, element) => {
+    let taskArray = JSON.parse(localStorage.getItem(key)); // Ambil array dari localStorage
+    element.text(taskArray.length); // Tampilkan jumlah elemen dalam array
+  };
+
+
+  // create unique id
+  const createId = () => {
+    // get timestamp
+    return +new Date();
+  };
+
+  // 
+  const getCheckedRadioValue = () => {
+    // takes the checked radio button and gets its value
+    const checkedValue = $("input.category:checked").val();
+    return checkedValue || null; // Kembalikan nilai atau null jika tidak ada yang dicentang
+  };
+
+
+  // define task object data
+  const generateTaskObject = (id, title, category, isCompleted) => {
     return {
-        id,
-        title,
-      }
+      id,
+      title,
+      category,
+      isCompleted,
+    };
   };
+
+  // Fungsi untuk menambahkan task baru ke DOM dan localStorage
+  const addTaskWork = () => {
+    // get unique id
+    const generateId = createId();
+
+    const taskCategory = getCheckedRadioValue();
+    
+    const newTaskWork = generateTaskObject(
+      generateId,
+      $("#input-new-task").val(),
+      getCheckedRadioValue(),
+      false,
+    );
+
+    if (newTaskWork) {
+      let task = JSON.parse(localStorage.getItem(`task${taskCategory}`)) || [];
+      task.push(newTaskWork);
+     // Simpan kembali ke localStorage sesuai dengan kategori
+     localStorage.setItem(`task${taskCategory}`, JSON.stringify(task));
+    }
+  };
+
+  $("#btn-submit-add-task").on("click", function () {
+    addTaskWork();
+  });
+
+    // Update dom count of task
+    updateTaskCount("taskWork", $("#task-work"));
+    updateTaskCount("taskStudy", $("#task-study"));
+    updateTaskCount("taskSport", $("#task-sport"));
+    updateTaskCount("taskFun", $("#task-fun"));
+    updateTaskCount("taskFamily", $("#task-family"));
+    updateTaskCount("taskOther", $("#task-other"));
   
-
-
-  checkTotalTask();
 });
-
-  
