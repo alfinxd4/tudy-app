135c46adc4d589b0f6b5517f1a6ef106118423cc
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2024-09-09 11:18:01 +0700

    final

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,66 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const glob = require("glob");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const glob = require('glob');
 
-const htmlFiles = glob.sync("./src/**/*.html"); // find all .html in src folder
+const htmlFiles = glob.sync('./src/**/*.html'); // Find all .html in src folder
 
-console.log("HTML Files Found:", htmlFiles); // found .html log files
+console.log('HTML Files Found:', htmlFiles); // Log found .html files
 
-// create an instance of HtmlWebpackPlugin for each .html file
+// Create an instance of HtmlWebpackPlugin for each .html file
 const htmlPlugins = htmlFiles.map(
   (file) =>
     new HtmlWebpackPlugin({
-      inject: "body", // ensure the script is loaded at the end of the body
+      inject: 'body', // Ensure the script is loaded at the end of the body
       template: file, // File .html as template
-      filename: path.relative("./src", file), // output in dist folder with same file name
+      filename: path.relative('./src', file), // Output in dist folder with same file name
     }),
 );
 
-module.exports = {
-  mode: "production", // webpack  mode build  
-  entry: {
-    app: "./src/js/app.js", // entry point input .js
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"), // entry output .js (dist/js)
-    filename: "js/[name].bundle.js",   
-  //  
-    assetModuleFilename: 'assets/images/[name][ext]', // entry output .img (dist/assets)
-  },
-  plugins: [
-    ...htmlPlugins, // HtmlWebpackPlugin instances
-  ],
-  module: {
-    // rules style
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          "css-loader",
-          "postcss-loader", 
-        ],
-      },
-     // rules images
-     {
-      test: /\.(png|jpg|jpeg|gif|svg)$/i, // type of img
-      type: 'asset/resource',  // use asset/resource for build image
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production'; // Determine if in production mode
+
+  return {
+    mode: isProduction ? 'production' : 'development', // Set Webpack mode
+    entry: {
+      app: './src/js/app.js', // Entry point for JavaScript
     },
+    output: {
+      path: path.resolve(__dirname, 'dist'), // Output directory
+      filename: 'js/[name].bundle.js', // Output JavaScript file
+      assetModuleFilename: 'assets/images/[name][ext]', // Output images to dist/assets
+    },
+    plugins: [
+      ...htmlPlugins, // HtmlWebpackPlugin instances
     ],
-  },
-  // config webpack-dev-server
-  devServer: {
-    client: {
-      overlay: {
-        errors: true,
-        warnings: false,
-      },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            !isProduction ? 'style-loader' : 'null-loader', 
+            'css-loader', // Load CSS
+            'postcss-loader', // Post-process CSS with PostCSS (includes Tailwind)
+          ],
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|svg)$/i, // Image types
+          type: 'asset/resource', // Use asset/resource for building images
+        },
+      ],
     },
-  },
+    // Config webpack-dev-server
+    devServer: {
+      client: {
+        overlay: {
+          errors: true,
+          warnings: false,
+        },
+      },
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+      compress: true,
+      port: 9000,
+    },
+  };
 };
